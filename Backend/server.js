122770fc532aa129c4a4ce3e198589d4d53a3d01
skyyy <aakashprajapati2457@gmail.com>
2026-02-15 19:04:22 +0530

    removed comments

--- Backend/server.js
+++ Backend/server.js
@@ -9,17 +9,17 @@ connectDB();
 
 const app = express();
 
-// MIDDLEWARE FIRST
+ 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ROUTES AFTER
+ 
 app.use("/api/content", contentRoutes);
 
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
